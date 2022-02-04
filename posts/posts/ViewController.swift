a6c555d82d451e288093d971a73eedf77f273e7c
Jose Tovar <jose.tovar@mercadolibre.com.co>
2022-02-04 18:55:59 -0500

    init project

---
+++ posts/posts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  posts
+//
+//  Created by Jose Tovar on 4/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
